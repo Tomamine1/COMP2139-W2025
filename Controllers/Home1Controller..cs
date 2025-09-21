468a43deeea75d085a03f3abdb09fd602214be45
Tomamine1 <101579629@georgebrown.ca>
2025-09-21 11:23:07 -0400

    Initial project setup

---
+++ Controllers/Home1Controller..cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace COMP2139_ICE.Controllers;
+
+    public class Home1Controller : Controller
+    {
+        // GET: Home1Controller_
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+
